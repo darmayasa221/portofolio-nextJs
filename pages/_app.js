73bf1c84211a1ed2acdc0cb739a079a86f7cdc87
darmayasa221 <darmayasa221@gmail.com>
2021-07-23 06:23:17 +0800

    version 1.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Fragment } from 'react'
+import Head from 'next/head';
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Fragment>
+      <Head>
+      <title>Darma Yasa</title>
+      <meta name='description' content='this is my Portofolio using Next Js'/>
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    </Fragment>
 }
-
 export default MyApp
